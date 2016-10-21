5dcba52d5b52d5847abfd5e5fa5ac33ffb2a7bbe
Uniericuni <uniericuni@gmail.com>
2016-10-21 00:51:05 -0400

    README.md

--- demo.m
+++ demo.m
@@ -2,8 +2,8 @@ close all;
 clear;
 clc;
 
-imRef = imread('/Users/eric/Desktop/JAPAN_sakuraNkimono/IMG_4132.jpg');
-folder = '/Users/eric/Pictures/AmberOnScreen/';
+imRef = imread('path/to/the/referenceImage');
+folder = 'path/to/the/photoColectionFolder/';
 out = imMontage(imRef, folder, 9, 0.4, 0.3);
 imshow(out);
 imwrite(out, 'demo.png');
